471782465e8afbf80e19ce1c8a2e5d4620829379
wenjie.wang <wenjie.wang@aispeech.com>
2017-01-10 16:44:48 +0800

    first run

--- src/client.c
+++ src/client.c
@@ -6,33 +6,32 @@
 #include <string.h>
 #include <memory.h>
 
-#include "include/comm.h"
-
 const char* ipaddr = "127.0.0.1";
+#define SERV_PORT 5000
 
 int main(void)
 {
     int fd;
     if((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        log_e("socket create error");
+        perror("socket create error");
     }
 
     struct sockaddr_in  serveraddr;
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
 
-    serveraddr.sin_port = htons(port);
+    serveraddr.sin_port = htons(SERV_PORT);
     //pointer to network
     inet_pton(AF_INET, ipaddr, &serveraddr.sin_addr.s_addr);
     //调用connect指定服务器的ip
     if(connect(fd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0)
     {
-        log_e("connect error");
+        perror("connect error");
     }
 
     if (send(fd, "Hello Arvin!", 12, 0) < 0){
-        log_e("send");
+        perror("send");
     }
 
     char buffer[1024];
@@ -40,10 +39,10 @@ int main(void)
     size_t size;
     if((size = recv(fd, buffer, sizeof(buffer), 0)) < 0)
     {
-        log_e("read error");
+        perror("read error");
     }
     
-    log_i("client read content: %s\n", buffer);
+    printf("client read content: %s\n", buffer);
 
     close(fd);
 
